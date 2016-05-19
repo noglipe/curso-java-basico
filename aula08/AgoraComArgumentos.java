9ca026d72bcf67ae74522732e7a49a04d7f5dcf2
Filipe Nogueira <nog.lipe@gmail.com>
2016-05-19 18:50:07 -0300

    Filipe Aula 08

---
+++ aula08/AgoraComArgumentos.java
@@ -0,0 +1,5 @@
+class AgoraComArgumentos {
+	public static void main(String[] args){
+		System.out.println("Vc digitou >> " + args[0]);
+	}
+}
---
+++ aula08/Programa1.java
@@ -0,0 +1,5 @@
+class Programa1 {
+	public static void main(String[] args){
+		System.out.println("Meu primeiro programinha :)");
+	}
+}
