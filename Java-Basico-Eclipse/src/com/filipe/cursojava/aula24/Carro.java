f9b90d7965304ff13eb8b3283afef9afa04fb707
Filipe Nogueira <nog.lipe@gmail.com>
2016-05-20 13:18:04 -0300

    Filipe Aula24 - Eclipse

---
+++ Java-Basico-Eclipse/src/com/filipe/cursojava/aula24/Carro.java
@@ -0,0 +1,13 @@
+package com.filipe.cursojava.aula24;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numeroDePassageiros;
+	double capacidadeDeCombustvel;
+	double comsumoDeCombustivel;
+	
+	
+
+}
