bde779f2d25c4f2d368298abae6b1c2daa646d10
Filipe Nogueira <nog.lipe@gmail.com>
2016-05-19 21:27:30 -0300

    Filipe Aula13

---
+++ curso-java-basico/src/com/filipe/cursojava/aula13/OperadoresAritimeticos.java
@@ -0,0 +1,33 @@
+package com.filipe.cursojava.aula13;
+
+public class OperadoresAritimeticos {
+    
+    public static void main(String[] args){
+        
+        int resultado = 1+2;
+        System.out.println(resultado);
+        
+        resultado = resultado -1;
+        System.out.println(resultado);
+        
+        resultado = resultado +2;
+        System.out.println(resultado);
+        
+        resultado = resultado *2;
+        System.out.println(resultado);
+        
+        resultado = resultado / 2;
+        System.out.println(resultado);
+        
+        resultado = resultado % 7;
+        System.out.println(resultado);
+        
+        String primeiroNome = "Esta é ";
+        String segundoNome = "uma String concatenada!";
+        String terceiroNome = primeiroNome + segundoNome;
+        System.out.println(terceiroNome);
+        
+        
+    }
+    
+}
