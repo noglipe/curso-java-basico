10cd56d51a8d82f8477d343087b528d3a2508358
Filipe Nogueira <nog.lipe@gmail.com>
2016-05-19 21:37:03 -0300

    Filipe Exercicos aula13

---
+++ curso-java-basico/src/com/filipe/cursojava/exercicios/aula13/Exercicio1.java
@@ -0,0 +1,11 @@
+package com.filipe.cursojava.exercicios.aula13;
+
+public class Exercicio1 {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Alo Mundo");
+        
+    }
+    
+}
---
+++ curso-java-basico/src/com/filipe/cursojava/exercicios/aula13/Exercicio2.java
@@ -0,0 +1,19 @@
+package com.filipe.cursojava.exercicios.aula13;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    
+    public static void main(String[] args){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Informe um numero");
+        int numero = scan.nextInt();
+        System.out.println("O numero informado foi: " + numero);
+        
+        
+    }
+            
+    
+}
