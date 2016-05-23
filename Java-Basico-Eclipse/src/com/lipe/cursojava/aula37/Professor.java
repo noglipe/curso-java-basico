12345bded14bed2ec64a24b45e580ac41ae3ad7c
Filipe Noguera <Filipe Noguera>
2016-05-23 11:43:49 -0300

    Filipe Aula 38

---
+++ Java-Basico-Eclipse/src/com/lipe/cursojava/aula37/Professor.java
@@ -0,0 +1,35 @@
+package com.lipe.cursojava.aula37;
+
+public class Professor extends Pessoa {
+
+
+	private double salario;
+	private String depatarmento;
+	private String nomeCurso;
+	
+	
+	public double getSalario() {
+		return salario;
+	}
+	public String getDepatarmento() {
+		return depatarmento;
+	}
+	public void setDepatarmento(String depatarmento) {
+		this.depatarmento = depatarmento;
+	}
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public double calcularSalarioLiquido(){
+		return 0;
+	}
+	
+	
+}
