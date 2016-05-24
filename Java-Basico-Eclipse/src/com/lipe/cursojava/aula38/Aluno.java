38fd87eb873fef1481f0cd3279e3101205418e10
Filipe Nogueira <nog.lipe@gmail.com>
2016-05-24 13:52:48 -0300

    Arrumando estrutura de pastas 2

---
+++ Java-Basico-Eclipse/src/com/lipe/cursojava/aula38/Aluno.java
@@ -0,0 +1,50 @@
+package com.lipe.cursojava.aula38;
+
+public class Aluno extends Pessoa{
+
+	private String curso;
+	private double notas[];
+	
+	
+	
+	public Aluno(String nome, String endereco, String telefone, String curso) {
+		super(nome, endereco, telefone);
+		this.curso = curso;
+	}
+	
+	public Aluno(String curso, double[] notas) {
+		super();
+		this.curso = curso;
+		this.notas = notas;
+	}
+	
+	public String getCurso() {
+		return curso;
+	}
+	public void setCuso(String curso) {
+		this.curso = curso;
+	}
+	public double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+	
+	public double calcularMedia(){
+		return 0;
+	}
+	public boolean cerificarAprovato(){
+		return true;		
+	}
+
+	public Aluno() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	
+	
+}
